20e05e6bea06e65f7f3802508b1a57a9968997f3
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-24 01:31:58 -0700

    first commit

--- lib/view/screen/profile/profile_screen.dart
+++ lib/view/screen/profile/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:delivery/view/common_widget/common_button.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -6,57 +7,67 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          CircleAvatar(
-            backgroundImage: AssetImage(""),
-            radius: 80,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+             // SizedBox(height: 15,),
+              CircleAvatar(
+                backgroundImage: AssetImage(""),
+                radius: 80,
+              ),
+              CommonButton(
+                  buttonName: "Change Profile", buttonWidth: 250, onTap: () {}),
+              SizedBox(height: 10),
+              Container(
+                height: 50,
+                width: MediaQuery.sizeOf(context).width,
+                color: Colors.grey,
+                child: Row(
+                  children: [
+                    Text("fiest name"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 50,
+                width: MediaQuery.sizeOf(context).width,
+                color: Colors.grey,
+                child: Row(
+                  children: [
+                    Text("last name"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 50,
+                width: MediaQuery.sizeOf(context).width,
+                color: Colors.grey,
+                child: Row(
+                  children: [
+                    Text("phone"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                height: 50,
+                width: MediaQuery.sizeOf(context).width,
+                color: Colors.grey,
+                child: Row(
+                  children: [
+                    Text("password"),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              CommonButton(buttonName: "Save", buttonWidth: 150, onTap: () {}),
+            ],
           ),
-          SizedBox(height: 10),
-          Container(
-            height: 50,
-            width: MediaQuery.sizeOf(context).width,
-            color: Colors.grey,
-            child: Row(
-              children: [
-                Text("fiest name"),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: 50,
-            width: MediaQuery.sizeOf(context).width,
-            color: Colors.grey,
-            child: Row(
-              children: [
-                Text("last name"),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: 50,
-            width: MediaQuery.sizeOf(context).width,
-            color: Colors.grey,
-            child: Row(
-              children: [
-                Text("phone"),
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            height: 50,
-            width: MediaQuery.sizeOf(context).width,
-            color: Colors.grey,
-            child: Row(
-              children: [
-                Text("password"),
-              ],
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
