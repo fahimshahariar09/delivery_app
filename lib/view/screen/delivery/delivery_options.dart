2c595148dfc1083910e7dee1e82881ec27523301
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-09-29 23:58:52 -0700

    first commit

---
+++ lib/view/screen/delivery/delivery_options.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DeliveryOptions extends StatelessWidget {
+  const DeliveryOptions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
