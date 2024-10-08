458e46c193814e1fb4f8ba8bbebebfe916e47c4f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-07 22:28:32 -0700

    first commit

--- lib/view/screen/auth/widget/email_text_field.dart
+++ lib/view/screen/auth/widget/email_text_field.dart
@@ -18,15 +18,16 @@ class EmailTextField extends StatelessWidget {
         ),
         errorBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: Colors.red,width: 1),
+          borderSide: const BorderSide(color: Colors.red, width: 1),
         ),
       ),
-      validator: (value){
-        if(value == '' || value == null){
+      validator: (value) {
+        if (value == '' || value == null) {
           return "Email can't be empty";
-        }else if(!(value.toString().contains('.') || value.contains('@'))){
+        } else if (!(value.toString().contains('.') || value.contains('@'))) {
           return "please your valid email";
         }
+        return null;
       },
     );
   }
