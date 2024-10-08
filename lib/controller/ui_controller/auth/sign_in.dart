497f0e1e1425a0a95786d4551add22bd0254bd3e
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-07 22:29:58 -0700

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -5,4 +5,5 @@ import 'package:get/get.dart';
 
 class SignInController extends GetxController{
   TextEditingController emailController =TextEditingController();
+  TextEditingController passController =TextEditingController();
 }
