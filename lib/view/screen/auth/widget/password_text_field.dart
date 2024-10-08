f5485c33e6c31ff2d6fd3b736e3c75ba1627c27d
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-07 22:31:15 -0700

    first commit

--- lib/view/screen/auth/widget/password_text_field.dart
+++ lib/view/screen/auth/widget/password_text_field.dart
@@ -6,7 +6,21 @@ class PasswordTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.red,width: 1)
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
     );
   }
 }
