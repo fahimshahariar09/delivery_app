e6bc11bfaac129e1ac5028853962fcf914b5b03b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-09 22:10:55 -0700

    first commit

--- lib/view/screen/auth/sign_in.dart
+++ lib/view/screen/auth/sign_in.dart
@@ -1,3 +1,6 @@
+import 'package:delivery/view/common_widget/common_button.dart';
+import 'package:delivery/view/screen/auth/widget/email_text_field.dart';
+import 'package:delivery/view/screen/auth/widget/password_text_field.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget {
@@ -5,6 +8,22 @@ class SignIn extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              EmailTextField(),
+              SizedBox(height: 15),
+              PasswordTextField(),
+              SizedBox(height: 15),
+              CommonButton(buttonName: "Sign In", onTap: () {})
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
