eee29ea315899b87da2aefddbc6ca5a4a7611772
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-09 22:33:13 -0700

    first commit

--- lib/controller/ui_controller/auth/sign_in.dart
+++ lib/controller/ui_controller/auth/sign_in.dart
@@ -1,9 +1,14 @@
-
-
+import 'package:delivery/controller/api_controller/auth/sign_in.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class SignInController extends GetxController{
-  TextEditingController emailController =TextEditingController();
-  TextEditingController passwordController =TextEditingController();
+class SignInController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  RxBool isLoading = true.obs;
+
+  signinFun() async {
+    await SignInService.signinService(
+        email: emailController.text, password: passwordController.text);
+  }
 }
