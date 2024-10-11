bc0b6914fb5c6ec55f0b80ce703175572233e3e8
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-10 23:28:23 -0700

    first commit

--- lib/controller/ui_controller/auth/sign_up.dart
+++ lib/controller/ui_controller/auth/sign_up.dart
@@ -1,13 +1,34 @@
+import 'package:delivery/controller/api_controller/auth/sign_up.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class SignUpController extends GetxController {
-
   final formKey = GlobalKey<FormState>();
-  TextEditingController nameController =TextEditingController();
-  TextEditingController phoneController =TextEditingController();
-  TextEditingController emailController =TextEditingController();
-  TextEditingController passwordController =TextEditingController();
-  TextEditingController conpasswordController =TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController conpasswordController = TextEditingController();
   RxBool isLoading = true.obs;
+
+  signUpFun() async {
+    isLoading.value = true;
+    bool status = await SignUpService.signupservice(
+        name: nameController.text,
+        phone: phoneController.text,
+        email: emailController.text,
+        password: passwordController.text,
+        conpassword: conpasswordController.text);
+    isLoading.value = false;
+  }
+
+  @override
+  void onInit() {
+    nameController.text = "fahim";
+    phoneController.text = "1254845253";
+    emailController.text = "fahim@gmail.com";
+    passwordController.text = "12345678";
+    conpasswordController.text = "12345678";
+    super.onInit();
+  }
 }
