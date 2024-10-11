341c60a8ffce169049f9341f398351ef645c0b8f
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-10 23:38:37 -0700

    first commit

--- lib/view/screen/auth/sign_up.dart
+++ lib/view/screen/auth/sign_up.dart
@@ -1,4 +1,5 @@
 import 'package:delivery/controller/ui_controller/auth/sign_up.dart';
+import 'package:delivery/view/common_widget/common_button.dart';
 import 'package:delivery/view/screen/auth/widget/con_password_text_field.dart';
 import 'package:delivery/view/screen/auth/widget/email_text_field.dart';
 import 'package:delivery/view/screen/auth/widget/name_text_field.dart';
@@ -12,7 +13,7 @@ class SignUp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    SignUpController signUpController =Get.put(SignUpController());
+    SignUpController signUpController = Get.put(SignUpController());
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -20,15 +21,22 @@ class SignUp extends StatelessWidget {
           child: Column(
             children: [
               SizedBox(height: 25),
-              NameTextField(nameController: signUpController.nameController,),
+              NameTextField(
+                nameController: signUpController.nameController,
+              ),
               SizedBox(height: 15),
               PhoneTextField(phoneController: signUpController.phoneController),
               SizedBox(height: 15),
               EmailTextField(emailController: signUpController.emailController),
               SizedBox(height: 15),
-              PasswordTextField(passwordController: signUpController.passwordController),
+              PasswordTextField(
+                  passwordController: signUpController.passwordController),
               SizedBox(height: 15),
-              ConPasswordTextField(conpasswordController: signUpController.conpasswordController),
+              ConPasswordTextField(
+                  conpasswordController:
+                      signUpController.conpasswordController),
+              SizedBox(height: 20),
+              CommonButton(buttonName: "Sign Up", onTap: () {}),
             ],
           ),
         ),
