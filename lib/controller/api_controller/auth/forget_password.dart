7c6fa3c294f89e6cdc79566530639f90137ecc2a
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-11 23:43:14 -0700

    first commit

--- lib/controller/api_controller/auth/forget_password.dart
+++ lib/controller/api_controller/auth/forget_password.dart
@@ -1,9 +1,9 @@
 import 'dart:developer';
 
 class ForgetPasswordService {
-  static Future forgetpasswordService() async {
+  static Future forgetpasswordService({required String email}) async {
     try {
-      await Future.delayed(Duration(seconds: 3));
+      await Future.delayed(const Duration(seconds: 3));
       return true;
     } catch (e) {
       log("error $e");
--- lib/controller/ui_controller/auth/forget_password.dart
+++ lib/controller/ui_controller/auth/forget_password.dart
@@ -1,12 +1,31 @@
-
-
-
+import 'package:delivery/controller/api_controller/auth/forget_password.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class ForgetPasswordController extends GetxController{
+class ForgetPasswordController extends GetxController {
+  TextEditingController emailController = TextEditingController();
+  RxBool isLoading = true.obs;
 
-  TextEditingController emailController =TextEditingController();
-  RxBool isLoading =true.obs;
-  
+  forgetpasswordFun() async {
+    isLoading.value = true;
+    bool status = await ForgetPasswordService.forgetpasswordService(
+        email: emailController.text);
+    isLoading.value = true;
+
+    if (status) {
+      return;
+    }
+  }
+
+  @override
+  void onInit() {
+    emailController.text = "fahim@gmail.com";
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    super.dispose();
+  }
 }
