74911086cea6faac968f6c1382b4e25dcd26ac04
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-12 23:07:16 -0700

    first commit

--- lib/view/screen/auth/widget/verify_field.dart
+++ lib/view/screen/auth/widget/verify_field.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class VerifyField extends StatelessWidget {
-  const VerifyField({super.key});
+  const VerifyField({super.key, required this.verifiyController});
+
+  final TextEditingController verifiyController;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SizedBox(
+      height: 50,
+      width: 50,
+      child: TextField(
+        controller: verifiyController,
+        decoration: InputDecoration(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.grey),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(color: Colors.grey),
+            ),
+            fillColor: Colors.grey),
+      ),
+    );
   }
 }
