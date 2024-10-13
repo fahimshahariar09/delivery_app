82bdc8342f7f45f16413949eaa5d5485e37707e3
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-12 22:29:26 -0700

    first commit

--- lib/view/screen/auth/forget_password.dart
+++ lib/view/screen/auth/forget_password.dart
@@ -1,15 +1,20 @@
+import 'package:delivery/controller/ui_controller/auth/forget_password.dart';
 import 'package:delivery/view/screen/auth/widget/email_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ForgetPassword extends StatelessWidget {
   const ForgetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ForgetPasswordController forgetPasswordController =
+        Get.put(ForgetPasswordController());
     return Scaffold(
       body: Column(
         children: [
-          // EmailTextField(emailController: emailController)
+          EmailTextField(
+              emailController: forgetPasswordController.emailController),
           Text(
               "Don’t worry ! It happens. Please enter the phone number we will send the OTP in this phone number."),
         ],
