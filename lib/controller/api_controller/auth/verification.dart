107d677a183c5252e75b2b0dd6341ed1691c5c39
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-12 23:17:09 -0700

    first commit

---
+++ lib/controller/api_controller/auth/verification.dart
@@ -0,0 +1,10 @@
+import 'dart:developer';
+
+class VerificationService {
+  static Future verificationService({required String num,required String num1,required String num2,required String num3,}) async {
+    try {} catch (e) {
+      log("error $e");
+    }
+    return false;
+  }
+}
