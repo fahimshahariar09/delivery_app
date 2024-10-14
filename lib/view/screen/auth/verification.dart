e9ec2891d947bf1873e972816cbcdb8f55ac7db1
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-13 22:59:43 -0700

    first commit

--- lib/view/screen/auth/verification.dart
+++ lib/view/screen/auth/verification.dart
@@ -1,4 +1,5 @@
 import 'package:delivery/controller/ui_controller/auth/verification.dart';
+import 'package:delivery/view/common_widget/common_button.dart';
 import 'package:delivery/view/screen/auth/widget/verify_field.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,36 +13,57 @@ class Verification extends StatelessWidget {
         Get.put(VerificationController());
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                VerifyField(
-                  verifiyController: verificationController.numController,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  VerifyField(
+                    verifiyController: verificationController.numController,
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  VerifyField(
+                    verifiyController: verificationController.num1Controller,
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  VerifyField(
+                    verifiyController: verificationController.num2Controller,
+                  ),
+                  SizedBox(
+                    width: 25,
+                  ),
+                  VerifyField(
+                    verifiyController: verificationController.num3Controller,
+                  ),
+                ],
+              ),
+              SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Container(
+                      height: 40,
+                      width: 150,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Center(child: Text("Confram",)),
+                    ),
+                  ],
                 ),
-                SizedBox(
-                  width: 25,
-                ),
-                VerifyField(
-                  verifiyController: verificationController.num1Controller,
-                ),
-                SizedBox(
-                  width: 25,
-                ),
-                VerifyField(
-                  verifiyController: verificationController.num2Controller,
-                ),
-                SizedBox(
-                  width: 25,
-                ),
-                VerifyField(
-                  verifiyController: verificationController.num3Controller,
-                ),
-              ],
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
