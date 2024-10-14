af2d4dd4ac04f391517cccae1994ec645e8f046b
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-13 22:46:10 -0700

    first commit

--- lib/controller/ui_controller/auth/verification.dart
+++ lib/controller/ui_controller/auth/verification.dart
@@ -1,10 +1,21 @@
-
+import 'package:delivery/controller/api_controller/auth/verification.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class VerificationController extends GetxController{
-TextEditingController numController =TextEditingController();
-TextEditingController num1Controller =TextEditingController();
-TextEditingController num2Controller =TextEditingController();
-TextEditingController num3Controller =TextEditingController();
+class VerificationController extends GetxController {
+  TextEditingController numController = TextEditingController();
+  TextEditingController num1Controller = TextEditingController();
+  TextEditingController num2Controller = TextEditingController();
+  TextEditingController num3Controller = TextEditingController();
+  RxBool isLoading = true.obs;
+
+  verifyFun() async {
+    isLoading.value = true;
+    bool status = await VerificationService.verificationService(
+        num: numController.text,
+        num1: num1Controller.text,
+        num2: num2Controller.text,
+        num3: num3Controller.text);
+    isLoading.value = false;
+  }
 }
