755393e8387146aedf35f880ca7937a0e89601e4
Fahim Shahariar Shihab <146321499+fahimshahariar09@users.noreply.github.com>
2024-10-13 22:43:37 -0700

    first commit

--- lib/controller/ui_controller/auth/verification.dart
+++ lib/controller/ui_controller/auth/verification.dart
@@ -1,6 +1,10 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class VerificationController extends GetxController{
-
+TextEditingController numController =TextEditingController();
+TextEditingController num1Controller =TextEditingController();
+TextEditingController num2Controller =TextEditingController();
+TextEditingController num3Controller =TextEditingController();
 }
