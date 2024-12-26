77e7ca762a68200e2458c3229e8aa24c751952b6
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-25 21:25:03 -0800

    first commit

--- lib/utlis/common_funcation/common_snackbar_message.dart
+++ lib/utlis/common_funcation/common_snackbar_message.dart
@@ -1,14 +1,51 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class CommonSnackBarMessage{
-  static noInternetConnection(){
-    Get.snackbar("you are offline", "check internet connection",
-    backgroundColor: Colors.red.shade50,
+class CommonSnackBarMessage {
+  static noInternetConnection() {
+    Get.snackbar(
+      "you are offline",
+      "check internet connection",
+      backgroundColor: Colors.red.shade50,
       colorText: Colors.redAccent,
       snackStyle: SnackStyle.FLOATING,
-      duration: Duration(seconds: 2),
+      duration: const Duration(seconds: 2),
+      overlayBlur: 5,
+      icon: const Icon(
+        Icons.wifi_off_outlined,
+        color: Colors.red,
+      ),
+      snackPosition: SnackPosition.TOP,
     );
+  }
+
+  static errorMessage({String? titel, required String text}) {
+    Get.snackbar(
+      titel ?? "message",
+      text,
+      backgroundColor: Colors.red.shade50,
+      colorText: Colors.redAccent,
+      snackStyle: SnackStyle.FLOATING,
+      duration: const Duration(seconds: 2),
+      overlayBlur: 5,
+      icon: const Icon(
+        Icons.wifi_off_outlined,
+        color: Colors.red,
+      ),
+      snackPosition: SnackPosition.TOP,
+    );
+  }
+
+  static noInternetConnection() {
+    Get.snackbar("you are offline", "check internet connection",
+        backgroundColor: Colors.red.shade50,
+        colorText: Colors.redAccent,
+        snackStyle: SnackStyle.FLOATING,
+        duration: const Duration(seconds: 2),
+        overlayBlur: 5,
+        icon: const Icon(
+          Icons.wifi_off_outlined,
+          color: Colors.red,
+        ));
   }
 }
