c6a5beeca08669e66cac621c4d06cfb05d59b8cd
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-25 21:28:34 -0800

    first commit

--- lib/utlis/common_funcation/common_snackbar_message.dart
+++ lib/utlis/common_funcation/common_snackbar_message.dart
@@ -29,23 +29,23 @@ class CommonSnackBarMessage {
       duration: const Duration(seconds: 2),
       overlayBlur: 5,
       icon: const Icon(
-        Icons.wifi_off_outlined,
+        Icons.error,
         color: Colors.red,
       ),
       snackPosition: SnackPosition.TOP,
     );
   }
 
-  static noInternetConnection() {
-    Get.snackbar("you are offline", "check internet connection",
+  static successMessage({String? titel, required String text, IconData? icon}) {
+    Get.snackbar(titel ?? "successful", text,
         backgroundColor: Colors.red.shade50,
         colorText: Colors.redAccent,
         snackStyle: SnackStyle.FLOATING,
         duration: const Duration(seconds: 2),
         overlayBlur: 5,
         icon: const Icon(
-          Icons.wifi_off_outlined,
-          color: Colors.red,
+          Icons.verified_user_sharp,
+          color: Colors.white,
         ));
   }
 }
