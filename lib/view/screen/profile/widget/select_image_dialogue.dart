f2ce03f667ab439193ce15f9986de163686330b5
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-28 23:42:29 -0800

    first commit

---
+++ lib/view/screen/profile/widget/select_image_dialogue.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SelectImageDialogue extends StatelessWidget {
+  const SelectImageDialogue({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog();
+  }
+}
