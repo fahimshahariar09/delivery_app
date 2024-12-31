072bdc80740c60667df4d0a4dd7a6f797fde4df8
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-30 23:55:00 -0800

    first commit

--- lib/view/common_widget/image_view_widget.dart
+++ lib/view/common_widget/image_view_widget.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:photo_view/photo_view.dart';
 
 class ImageViewWidget extends StatelessWidget {
-  const ImageViewWidget({super.key});
+  const ImageViewWidget({super.key, required this.imageUrl});
+
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +13,14 @@ class ImageViewWidget extends StatelessWidget {
           child: SizedBox(
         height: MediaQuery.sizeOf(context).height,
         width: MediaQuery.sizeOf(context).width,
-            child: Center(
-              child: Hero(tag: "Das", child: PhotoView(imageProvider: NetworkImage(url))),
+        child: Center(
+          child: Hero(
+            tag: "Das",
+            child: PhotoView(
+              imageProvider: NetworkImage(imageUrl),
             ),
+          ),
+        ),
       )),
     );
   }
