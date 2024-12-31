cb86b6a26d2c6858c83f25f9dc462a75ad4cf33d
Fahim Shahariar Shihab <fahimshahariar20002@gmail.com>
2024-12-30 23:54:06 -0800

    first commit

---
+++ lib/view/common_widget/image_view_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+
+class ImageViewWidget extends StatelessWidget {
+  const ImageViewWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: SizedBox(
+        height: MediaQuery.sizeOf(context).height,
+        width: MediaQuery.sizeOf(context).width,
+            child: Center(
+              child: Hero(tag: "Das", child: PhotoView(imageProvider: NetworkImage(url))),
+            ),
+      )),
+    );
+  }
+}
--- lib/view/screen/profile/profile_screen.dart
+++ lib/view/screen/profile/profile_screen.dart
@@ -1,15 +1,40 @@
+import 'package:delivery/utlis/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Size size =MediaQuery.sizeOf(context);
+    return  Scaffold(
       body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-
+            Container(
+              alignment: Alignment.center,
+              width: size.width,
+              padding: EdgeInsets.only(top: 70,bottom: 20),
+              decoration: BoxDecoration(
+                color: AppColors.bg1LightColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  InkWell(
+                    onTap: (){
+                      Get
+                    },
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
